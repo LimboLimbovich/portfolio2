91783784e7b77c13d94c327462d1e7813631d18c
limbo <benz16082005@gmail.com>
2023-11-23 15:40:04 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".multiple-text", {
+    strings: [
+        "FullStack Developer", "Frontend Developer", "Backend Developer" 
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
